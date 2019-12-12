a50d819c0e9494f27cae0603c79020b5d8b765fe
Kalau90 <45683444+Kalau90@users.noreply.github.com>
2019-12-12 15:43:25 +0100

    Mysqli-object

--- airtable/testapi.php
+++ airtable/testapi.php
@@ -1,18 +1,13 @@
 <?php
+//$link = mysqli_connect(, "dynamiccases", 3306);
+// Create connection
+$conn = new mysqli("10.83.19.104", "root", "kathomads");
 
-$link = mysqli_connect("10.83.19.104", "root", "kathomads", "dynamiccases", 3306);
-
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
-mysqli_close($link);
+echo "Connected successfully";
 
 $airtable_login = array(
     'api_key'   => 'keyK9H1XTdu0nJB4j',
